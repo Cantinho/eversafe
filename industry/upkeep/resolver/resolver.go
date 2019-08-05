afc14d908814b51ae2ee2c8bf634ec25fbe53946
Samir Trajano Feitosa <sfeitosa@mcontigo.com>
2019-08-05 17:53:34 -0300

    add gin framework; missing fix middleware

--- industry/upkeep/resolver/resolver.go
+++ industry/upkeep/resolver/resolver.go
@@ -2,13 +2,18 @@ package resolver
 
 import (
 	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"math/rand"
 	"upkeep/generated"
 	"upkeep/models"
 )
 
 // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
-type Resolver struct{}
+type Resolver struct {
+	users []*models.User
+}
 
 func (r *Resolver) Mutation() generated.MutationResolver {
 	return &mutationResolver{r}
@@ -20,11 +25,43 @@ func (r *Resolver) Query() generated.QueryResolver {
 type mutationResolver struct{ *Resolver }
 
 func (r *mutationResolver) CreateUser(ctx context.Context, input models.NewUser) (*models.User, error) {
-	panic("not implemented")
+	user := &models.User{
+		ID:    fmt.Sprintf("T%d", rand.Int()),
+		Name:  input.Name,
+		Email: input.Email,
+	}
+	r.users = append(r.users, user)
+	return user, nil
 }
 
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Users(ctx context.Context) ([]*models.User, error) {
-	panic("not implemented")
+	_, err := GinContextFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return r.users, nil
+}
+
+//type userResolver struct{ *Resolver }
+//
+//func (r *userResolver) User(ctx context.Context, obj *models.User) (*models.User, error) {
+//	return &models.User{ID: obj.ID, Name: "name " + obj.Name, Email: "email " + obj.Email}, nil
+//}
+
+// Define a function to recover the gin.Context from the context.Context struct
+func GinContextFromContext(ctx context.Context) (*gin.Context, error) {
+	ginContext := ctx.Value("GinContextKey")
+	if ginContext == nil {
+		err := fmt.Errorf("could not retrieve gin.Context")
+		return nil, err
+	}
+
+	gc, ok := ginContext.(*gin.Context)
+	if !ok {
+		err := fmt.Errorf("gin.Context has wrong type")
+		return nil, err
+	}
+	return gc, nil
 }
--- industry/upkeep/server/server.go
+++ industry/upkeep/server/server.go
@@ -1,25 +1,88 @@
 package main
 
 import (
+	"context"
+	"github.com/99designs/gqlgen/handler"
 	"log"
-	"net/http"
 	"os"
+	//"log"
+	"github.com/gin-gonic/gin"
+	//"net/http
 	"upkeep/generated"
 	"upkeep/resolver"
-	"github.com/99designs/gqlgen/handler"
 )
 
-const defaultPort = "8080"
+const defaultPort = "9000"
+const defaultGraphqlEndpoint = "graphql"
+
+// At the Resolver level, gqlgen gives you access to the context.Context object.
+// One way to access the gin.Context is to add it to the context and retrieve it again.
+func GinContextToContextMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.WithValue(c.Request.Context(), "GinContextKey", c)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := handler.Playground("GraphQL", "/" + defaultGraphqlEndpoint)
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 
 func main() {
+	// Setting up Gin
+	// router := gin.Default() creates a default middleware.
+	router := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	//router.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.Recovery())
+
+	router.Use(GinContextToContextMiddleware())
+	router.POST("/" + defaultGraphqlEndpoint, graphqlHandler())
+	router.GET("/", playgroundHandler())
+
+	// By default it serves on :8080 unless a
+	// PORT environment variable was defined.
+	// router.Run()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
-
-	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}})))
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground handling by Gin", port)
+	router.Run(":" + port)
 }
+
+//func main() {
+//	port := os.Getenv("PORT")
+//	if port == "" {
+//		port = defaultPort
+//	}
+//
+//	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
+//	http.Handle("/query", handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}})))
+//
+//	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+//	log.Fatal(http.ListenAndServe(":"+port, nil))
+//}
