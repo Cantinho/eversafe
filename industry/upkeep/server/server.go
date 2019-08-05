cf0cf7d04d656e055ae1fdcf4a66c0c6ec125046
Samir Trajano Feitosa <sfeitosa@mcontigo.com>
2019-08-05 20:35:22 -0300

    add custom log; missing log size handling; missing custom middleware on resolver

--- industry/upkeep/server/server.go
+++ industry/upkeep/server/server.go
@@ -1,13 +1,22 @@
 package main
 
+
+/**
+ * go run github.com/99designs/gqlgen
+ * go run server/server.go
+ *
+ * Resolvers isn't auto generated after gqlgen.
+ */
+
 import (
 	"context"
+	"fmt"
 	"github.com/99designs/gqlgen/handler"
+	"github.com/gin-gonic/gin"
+	"io"
 	"log"
 	"os"
-	//"log"
-	"github.com/gin-gonic/gin"
-	//"net/http
+	"time"
 	"upkeep/generated"
 	"upkeep/resolver"
 )
@@ -54,7 +63,32 @@ func main() {
 	// Global middleware
 	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
 	// By default gin.DefaultWriter = os.Stdout
-	//router.Use(gin.Logger())
+	// Disable Console Coloer, you don't need console color when writing the logs to file.
+	gin.DisableConsoleColor()
+
+	// Logging to a file.
+	file, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(file)
+
+	// Use the following code if you need to write the logs to file and console at the same time.
+	// gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	router.Use(gin.Logger())
 
 	// Recovery middleware recovers from any panics and writes a 500 if there was one.
 	router.Use(gin.Recovery())
